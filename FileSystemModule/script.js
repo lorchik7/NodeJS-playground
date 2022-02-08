e4d6ba370aaedf31def25a636386ef08f331f04f
Lora Kisyova <llovchalieva@gmail.com>
2022-02-09 01:51:20 +0200

    Add append, write, delete options

--- FileSystemModule/script.js
+++ FileSystemModule/script.js
@@ -1,11 +1,34 @@
 const fs = require('fs');
 
+//READ
 fs.readFile("./hello.txt", (err, data) => {
 	if(err) {
-		console.log('error');
+		console.log('This is my brand new error message!');
 	}
 	console.log('Async', data.toString('utf8'));
 });
 
 const file = fs.readFileSync('./hello.txt');
 console.log('Sync', file.toString());
+
+//APPEND
+// fs.appendFile('./hello.txt', ' This is so cool!', err => {
+// 	if(err) {
+// 		console.log(err);
+// 	}
+// });
+
+//WRITE
+fs.writeFile('bye.txt', 'Sad to see you go!', err => {
+	if(err) {
+		console.log("Error");
+	}
+});
+
+//DELETE
+fs.unlink('./bye.txt', err => {
+	if(err) {
+		console.log(err);
+	}
+	console.log("Inception")
+});
